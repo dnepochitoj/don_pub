a987858e31a601016af5be7e42df5507b94fcae6
dnepochitoj <51474930+dnepochitoj@users.noreply.github.com>
2019-10-16 21:50:59 +0300

    Update ora_inst_block_detail.sql

--- admin/orcl/db/scripts/ora_inst_block_detail.sql
+++ admin/orcl/db/scripts/ora_inst_block_detail.sql
@@ -1,59 +1,51 @@
 --
 -- ora_inst_block_detail.sql v1.0
 --
+-- Formatted by SQLFormat.org (https://sqlformat.org)
+--
 -- Frequency: XX min
 --    Result: n>0 - Warning
 --   Message: Detailed information about blocking session(s)
 --   History:
 -- * v1.0 - Created by D.Nepochitoj
 
-SELECT
-  S.KSUUDLNA USERNAME
-, S.KSUSEUNM OSUSER
-, S.KSUSEMNM MACHINE
-, S.KSUSEPNM PROGRAM
-, TO_CHAR(S.KSUSELTM,'MM/DD/YYYY HH24:MI:SS') LOGON_TIME
-, FLOOR(S.KSUSEWTM/3600)||':'||FLOOR(MOD(S.KSUSEWTM,3600)/60)||':'||MOD(MOD(S.KSUSEWTM,3600),60) SECONDS_IN_WAIT
-, S.KSUSEACT ACTION
-, S.KSUSEAPP MODULE
-, DECODE(BITAND(S.KSUSEIDL, 11),
-              1, 'ACTIVE',
-              0, DECODE(BITAND(S.KSUSEFLG, 4096), 0, 'INACTIVE', 'CACHED'),
-              2, 'SNIPED',
-              3, 'SNIPED',
-                 'KILLED') STATUS
-, S.INDX SID
-, S.KSUSESER SERIAL#
-, E.KSLEDNAM EVENT
-, L.BLOCK
-  FROM X$KSUSE S
-     , X$KSLED E
-     , V$_LOCK L
- WHERE BITAND(S.KSSPAFLG, 1) != 0
-  AND BITAND(S.KSUSEFLG, 1) != 0
-  AND S.KSUSEOPC = E.INDX
-  AND L.SADDR=S.ADDR
+SELECT s.ksuudlna username,
+       s.ksuseunm osuser,
+       s.ksusemnm machine,
+       s.ksusepnm program,
+       to_char(s.ksuseltm, 'MM/DD/YYYY HH24:MI:SS') logon_time,
+       floor(s.ksusewtm/3600)||':'||floor(mod(s.ksusewtm, 3600)/60)||':'||mod(mod(s.ksusewtm, 3600), 60) seconds_in_wait,
+       s.ksuseact action,
+       s.ksuseapp MODULE,
+                  decode(bitand(s.ksuseidl, 11), 1, 'ACTIVE', 0, decode(bitand(s.ksuseflg, 4096), 0, 'INACTIVE', 'CACHED'), 2, 'SNIPED', 3, 'SNIPED', 'KILLED') status,
+                  s.indx sid,
+                  s.ksuseser serial#,
+                  e.kslednam event,
+                  l.block
+FROM x$ksuse s ,
+     x$ksled e ,
+     v$_lock l
+WHERE bitand(s.ksspaflg, 1) != 0
+  AND bitand(s.ksuseflg, 1) != 0
+  AND s.ksuseopc = e.indx
+  AND l.saddr=s.addr 
 --AND S.INDX IN  1350         -- TEST SESSION
   -- check if query exceeds time limit (default 600 sec)
-  AND S.KSUSEWTM >
-    NVL
-    (
-      (
-          SELECT MAX(V1) FROM JETDBA.MON_THRESHOLDS WHERE
-          (
-              INST_ID=0 OR INST_ID=(SELECT INSTANCE_NUMBER FROM V$INSTANCE)
-          )
-          AND STATNAME='INST_BLOCK'
-      ) , 600
-    )
+  AND s.ksusewtm > nvl (
+                          (SELECT max(v1)
+                           FROM jetdba.mon_thresholds
+                           WHERE (inst_id=0
+                                  OR inst_id=
+                                    (SELECT instance_number
+                                     FROM v$instance))
+                             AND statname='INST_BLOCK' ) , 600) 
   -- check if wait event is in exclude list
-  AND E.KSLEDNAM NOT IN       -- AND EVENT NOT IN
-  (
-      SELECT NAME FROM JETDBA.MON_EXCLUDE WHERE STATNAME='INST_BLOCK'
-  )
+  AND e.kslednam NOT IN -- AND EVENT NOT IN
+    (SELECT name
+     FROM jetdba.mon_exclude
+     WHERE statname='INST_BLOCK' )
   -- check if the lock is blocking another lock
-  AND L.BLOCK = 1
-/
+  AND l.block = 1; 
 
 -- Sample output:
 --     USERNAME  OSUSER  MACHINE     PROGRAM                         LOGON_TIME          SECONDS_IN_WAIT ACTION  MODULE                          STATUS    SID   SERIAL# EVENT                       BLOCK
