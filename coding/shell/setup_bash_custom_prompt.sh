f7f8d5116cb0375b6312bf51f0ecba2165db3007
dnepochitoj <51474930+dnepochitoj@users.noreply.github.com>
2023-06-25 15:20:33 +0300

    added for Mac and linux PS1

--- coding/shell/setup_bash_custom_prompt.sh
+++
@@ -1,2 +0,0 @@
-# setup_bash_custom_prompt.sh 
-export PS1="%{%F{033}%}%n%{%f%}@%{%F{green}%}%m %{%F{yellow}%}%d%{ # %f%}%"
---
+++ coding/shell/setup_custom_bash_prompt.sh
@@ -0,0 +1,8 @@
+# mac
+# black, white, yellow, green, red, blue, cyan, and magenta
+# vi ~/.zshrc
+export PS1="%{%F{cyan}%}%n%{%f%}@%{%F{magenta}%}%m %{%F{red}%}%d%{ # %f%}%"
+
+# linux
+# vi ~/.bashrc
+export PS1="\[\e[36m\]\u\[\e[m\]@\[\e[35m\]\h\[\e[m\] \[\e[31m\]\w\[\e[m\] # "
